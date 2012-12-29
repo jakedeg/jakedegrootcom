692cdefd11faf2c3548c7f3964da8ce2c48b88b1
Jake DeGroot <jakedeg@gmail.com>
2012-12-29 16:48:29 -0500

    Prettier preloader, Restore slider functionality.

--- wp-content/themes/jdgviewport/index.php
+++ wp-content/themes/jdgviewport/index.php
@@ -1,17 +1,11 @@
 <?php get_header(); ?>
 
-<?php
+	<script src=<?php get_template_directory_uri(); ?>"/wp-content/themes/jdgviewport/js/jquery-1.8.2.min.js"></script>
+	<script src=<?php get_template_directory_uri(); ?>"/wp-content/themes/jdgviewport/js/jquery-ui-1.8.20.custom.min.js"></script>
 
-function slider_scripts()  
-{  
-    // Register the script like this for a theme:  
-    wp_register_script( 'LiquidSlider', get_template_directory_uri() . '/js/jquery.liquid-slider-1.1.min.js', array( 'jquery', 'jquery-ui-core' ) );  
-    // For either a plugin or a theme, you can then enqueue the script:  
-    wp_enqueue_script( 'LiquidSlider' );  
-}  
-add_action( 'wp_enqueue_scripts', 'slider_scripts' );  
+ 
+	<script src=<?php get_template_directory_uri(); ?>"/wp-content/themes/jdgviewport/js/jquery.liquid-slider-1.1.min.js"></script> 
 
-?>
 	<link rel="stylesheet" type="text/css" media="screen" href=<?php get_template_directory_uri(); ?>"/wp-content/themes/jdgviewport/liquid-slider-1.1.css">
 
     <script>
